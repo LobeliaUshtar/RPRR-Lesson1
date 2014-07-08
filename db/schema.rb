9133837fba1ae7eef8a497ea8cb03d8c4c240b7b
Becky Osiecki <lobeliaushtar@gmail.com>
2014-07-08 15:27:58 -0400

    Assignment: Comment model + Comment associations
    Rapid Prototyping with Ruby on Rails - Lesson 1 #15-16

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140708162849) do
+ActiveRecord::Schema.define(version: 20140708191443) do
+
+  create_table "comments", force: true do |t|
+    t.text     "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "user_id"
+    t.integer  "post_id"
+  end
 
   create_table "posts", force: true do |t|
-    t.string   "title"
-    t.string   "url"
+    t.text     "title"
+    t.text     "url"
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
