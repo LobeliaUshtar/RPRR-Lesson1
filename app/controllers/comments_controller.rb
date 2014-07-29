aa860c19f832379b26f8333dd411a8930f6ecb4f
Becky Osiecki <lobeliaushtar@gmail.com>
2014-07-29 16:49:42 -0400

    Module Assignments #13+15
    Rapid Prototyping with Ruby on Rails - Lesson 4
    - Assignment: Voteable Module
    - Assignment: Sluggable Module

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -15,7 +15,7 @@ class CommentsController < ApplicationController
   end
 
   def vote
-    @comment = Comment.find_by slug: params[:id]
+    @comment = Comment.find(params[:id])
     @vote = Vote.create(voteable: @comment, creator: current_user, vote: params[:vote])
 
     respond_to do |format|
