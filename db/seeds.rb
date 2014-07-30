39b84a6becf06d3b3294aeab605e0407e55a7fa1
Becky Osiecki <lobeliaushtar@gmail.com>
2014-07-30 16:36:26 -0400

    Assignment: Simple Admin FIXED
    Rapid Prototyping with Ruby on Rails - Lesson 4 #19 FIXED
    updated seed for Heroku
     - 1 user (admin)
     - 1 post
     - 3 categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
 User.create!([
   {
     username: "gabby",
@@ -14,3 +6,18 @@ User.create!([
     role: "admin"
   }
 ])
+
+Category.create!([
+  {
+    name: "Ruby on Rails"
+  },
+  {
+    name: "HTML"
+  },
+  {
+    name: "CSS"
+  }
+])
+
+user = User.find_by(username: "gabby")
+user.posts.create!(title: "Coding on the Ruby Express", url: "http://codingontherubyexpress.tumblr.com", description: "A blog about what Becky is doing with her Ruby on Rails courses and things she is finding along the way.")
